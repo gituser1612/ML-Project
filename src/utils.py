549c49a4eed5c1689f5e9e2ed1e4911170b4187f
gituser1612 <amitkr.pandey2871@gmail.com>
2026-07-30 15:29:37 +0530

    Prediction Pipeline done

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+import pickle
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
@@ -13,7 +14,7 @@ def save_object(file_path,obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
@@ -44,4 +45,11 @@ def  evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e,sys)
-        
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
